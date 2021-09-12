2715cd084db02c5c614800e157722e1fad3a1c5a
maxwellgraeser <mgraeser22701@gmail.com>
2021-09-12 19:38:20 -0400

    Initial commit

---
+++ src/main/java/ComputingInterest.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 10 Solution
+ *  Copyright 2021 Maxwell Graeser
+ */
+
+import java.util.Scanner;
+public class ComputingInterest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the principal: ");
+        double principle = scan.nextDouble();
+        System.out.print("Enter the interest rate: ");
+        double rate = scan.nextDouble();
+        System.out.print("Enter the number of years: ");
+        int years = scan.nextInt();
+
+        double finalAmount = principle * (1 + ((rate / 100) * years));
+
+        System.out.println(String.format("After %d years at %.2f percent interest, the investment will be worth %.2f", years, rate, finalAmount));
+    }
+}
